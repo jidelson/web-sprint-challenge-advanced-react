a87f2cc60720a14777ac1a8b7fa73c207a07f9d4
jidelson <58210030+jidelson@users.noreply.github.com>
2020-07-10 14:18:55 -0700

    fixed one test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -10,14 +10,28 @@ test("form header renders", () => {
     expect(header).toBeInTheDocument()
 });
 
-test("form shows success message on submit with form details", () => {
-    const handler = jest.fn(e => e.preventDefault())
-    const {getByTestId} = render(
-        <form onSubmit={handler} data-testid="successMessage">
-            <button>Checkout</button>
-        </form>
-    )
-    fireEvent.submit(getByTestId('successMessage'))
-    expect(handler).toHaveBeenCalledTimes(1)
-    
+test("form shows success message on submit with form details",  () => {
+   render(<CheckoutForm />)
+   
+   const fnInput = screen.getByLabelText(/First Name:/i);
+   const lnInput = screen.getByLabelText(/Last Name:/i);
+   const addressInput = screen.getByLabelText(/Address:/i);
+   const cityInput = screen.getByLabelText(/City:/i);
+   const stateInput = screen.getByLabelText(/State:/i);
+   const zipInput = screen.getByLabelText(/Zip:/i); 
+
+   fireEvent.change(fnInput, { target: { value: 'Joe'}});
+   fireEvent.change(lnInput, { target: { value: 'Idelson'}});
+   fireEvent.change(addressInput, { target: { value: '1250 Walker Ave'}});
+   fireEvent.change(cityInput, { target: { value: 'Walnut Creek'}});
+   fireEvent.change(stateInput, { target: { value: 'CA'}});
+   fireEvent.change(zipInput, { target: { value: '94596'}});
+
+   const checkoutButton =  screen.getByTestId(/myButton/i);
+   fireEvent.click(checkoutButton);
+   
+   const successMessage =  screen.getByTestId(/successMessage/i);
+   expect(successMessage).toBeInTheDocument();
+
+
 });
